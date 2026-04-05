1af6cf5a57468a09c0b803d9139b0740bc20c67d
Zahraoui Saad <saadxzhr1@gmail.com>
2026-04-05 15:19:11 +0100

    components inside tabs

--- demandes_frontend/src/app/app.routes.ts
+++ demandes_frontend/src/app/app.routes.ts
@@ -1,9 +1,3 @@
 import { Routes } from '@angular/router';
-import { DemandesComponent } from './demandes/demandes-component/demandes-component';
-import { HistoriqueComponent } from './historique/historique-component/historique-component';
 
-export const routes: Routes = [
-    { path: 'demandes', component: DemandesComponent },
-    { path: 'historique', component: HistoriqueComponent },
-    { path: '', redirectTo: '/demandes', pathMatch: 'full' }
-];
+export const routes: Routes = []
